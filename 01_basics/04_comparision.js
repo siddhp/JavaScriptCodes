c2b6e36fe4988000c3d9b940d30d4538b3ea9ddd
siddhp <86943376+siddhp@users.noreply.github.com>
2023-10-21 08:29:07 +0000

    datatype summary

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -10,3 +10,4 @@ console.log(undefined <=0);
 
 console.log("2"===2);
 console.log("2"==2);
+
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,21 @@
+// Primitive
+
+// 7 types : String, Number, Boolean , null, undefined, Symbol, BigInt
+
+
+
+
+
+// Reference (Non Primitive)
+
+//Array, Objects, Functions
+
+const heros=["shaktiman", "krish"]
+let myObj={
+    name:"siddh",
+    age:22,
+}
+
+const myFunction=function(){
+console.log("Hello");
+}
