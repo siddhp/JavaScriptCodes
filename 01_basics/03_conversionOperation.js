0a09ba5ee438ebb87d8a5db16fde161103f340fb
siddhp <86943376+siddhp@users.noreply.github.com>
2023-10-21 08:16:24 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,8 +17,8 @@ let isLoggedIn=0
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 
-console.log( isLoggedIn);
-console.log( booleanIsLoggedIn);
+// console.log( isLoggedIn);
+// console.log( booleanIsLoggedIn);
 
 // 1 => true 0 => false
 //"siddh" =>true
@@ -27,5 +27,21 @@ console.log( booleanIsLoggedIn);
 let someNum=23
 
 let stringstr=String(someNum)
-console.log(stringstr);
-console.log(typeof stringstr);
+// console.log(stringstr);
+// console.log(typeof stringstr);
+
+
+
+//*********************************** Operation ***********************************
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+let str1="hello"
+let str2=" siddh"
+
+let str3=str1+str2
+
+console.log(str3);
+
