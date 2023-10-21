81b1032da617d1fce5e6dab9071e81ee4ced9c10
siddhp <86943376+siddhp@users.noreply.github.com>
2023-10-21 07:57:54 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score="33"
+
+// console.log(typeof score);
+
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber);
+
+//"33" => 33
+//"as21" => NaN
+
+// true= > 1
+//false => 0
+
+
+let isLoggedIn=0
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log( isLoggedIn);
+console.log( booleanIsLoggedIn);
+
+// 1 => true 0 => false
+//"siddh" =>true
+
+
+let someNum=23
+
+let stringstr=String(someNum)
+console.log(stringstr);
+console.log(typeof stringstr);
