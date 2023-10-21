96a545da24f7f741e6201bb2f2d2e829ea79dd1f
siddhp <86943376+siddhp@users.noreply.github.com>
2023-10-21 07:47:23 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=12;
+let accountEmail="siddh@gmail.com"
+var accountNumber="12345"
+
+accountCity="ahmedabad"
+
+//not allowed
+// accountId=3
+accountEmail="sid12@gamil.com"
+accountNumber="2222"
+accountCity="modasa"
+let accountState
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountNumber,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all JS code as newer version
+
+let name="Siddh"
+let age=22
+let isLoggedIn=false
+
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
